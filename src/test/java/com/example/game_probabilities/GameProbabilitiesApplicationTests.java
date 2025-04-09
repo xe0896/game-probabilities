e6c9ba2ab9cf2a30db4c90568c15c8d53287d279
Sanim Ahmed <sanimahmed26@gmail.com>
2025-04-09 18:53:15 +0100

    Spring boot initalized the program

---
+++ src/test/java/com/example/game_probabilities/GameProbabilitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.game_probabilities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameProbabilitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
